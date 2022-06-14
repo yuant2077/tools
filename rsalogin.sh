e04448924bb2fdb0ac00d14b4c80eaa334491eef
yuant2077 <107481445+yuant2077@users.noreply.github.com>
2022-06-14 23:02:47 +0800

    Create rsalogin.sh

---
+++ rsalogin.sh
@@ -0,0 +1,8 @@
+wget https://github.com/yuant2077/tools/raw/main/ssh.pub
+cat ssh.pub >> ~/.ssh/authorized_keys
+sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config && \
+sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config && \
+sed -i 's/^.*RSAAuthentication.*/RSAAuthentication yes/g' /etc/ssh/sshd_config && \
+sed -i 's/^.*PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config && \
+sed -i 's/^.*ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd
